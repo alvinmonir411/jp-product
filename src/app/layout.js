566243224e73f9e29f9d910693cf23f42d6e7aff
alvinmonir411 <moniruzzaman710@outlook.com>
2025-07-28 20:27:06 +0600

    initial setup done with nextjs tailwind and deisy ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import HeroSlider from "./components/HeroSlider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
+        <HeroSlider />
         {children}
       </body>
     </html>
